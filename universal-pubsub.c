cff6ded5bf87df4b18a7d34a39a0e063c56da02e
Stefan de Konink <stefan@konink.de>
2012-10-08 17:28:48 +0200

    Add ZeroMQ3 compatible code based on the work of Peter Fokkinga.

--- universal-pubsub.c
+++ universal-pubsub.c
@@ -3,7 +3,7 @@
  * consisting of an envelope and its data, is broadcasted to
  * all connected serviceproviders.
  *
- * Requirements: zeromq2
+ * Requirements: zeromq2 or zeromq3.2
  * gcc -lzmq -o universal-pubsub universal-pubsub.c
  *
  * Changes:
@@ -30,7 +30,12 @@ int main (int argc, char *argv[]) {
 
     /* Apply a high water mark at the PubSub */
     uint64_t hwm   = 255;
+    #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(3,0,0)
+    zmq_setsockopt(pubsub, ZMQ_SNDHWM, &hwm, sizeof(hwm));
+    zmq_setsockopt(pubsub, ZMQ_RCVHWM, &hwm, sizeof(hwm));
+    #else
     zmq_setsockopt(pubsub, ZMQ_HWM, &hwm, sizeof(hwm));
+    #endif
 
     zmq_bind (pubsub,   argv[2]);
     zmq_bind (receiver, argv[1]);
@@ -43,14 +48,26 @@ int main (int argc, char *argv[]) {
             zmq_msg_t part;
             int rc = zmq_msg_init (&part);
             assert (rc == 0);
+
             /* Block until a message is available to be received from the socket */
+	    #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(3,0,0)
+	    rc = zmq_recvmsg (subscriber, &part, 0);
+	    #else
             rc = zmq_recv (receiver, &part, 0);
+	    #endif
             assert (rc == 0);
+
             /* Determine if more message parts are to follow */
             rc = zmq_getsockopt (receiver, ZMQ_RCVMORE, &more, &more_size);
             assert (rc == 0);
+
             /* Send the message, when more is set, apply the flag, otherwise don't */
+	    #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(3,0,0)
+	    zmq_sendmsg (pubsub, &part, (more ? ZMQ_SNDMORE : 0));
+	    #else
             zmq_send (pubsub, &part, (more ? ZMQ_SNDMORE : 0));
+	    #endif
+
             zmq_msg_close (&part);
         } while (more);
     }
